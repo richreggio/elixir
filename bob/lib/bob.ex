318795d3cb3820fe24ba56cdcb56bbfbe1c11d1e
Richard Bickford <rzacharski05@gmail.com>
2020-10-09 18:27:58 -0400

    bob mostly finished

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,7 +1,31 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      true -> raise "Your implementation goes here"
+      yelled_question?(input) -> "Calm down, I know what I'm doing!"
+      yelled_at?(input) -> "Whoa, chill out!"
+      asked_question?(input) -> "Sure."
+      nonsense_statement?(input) -> "Fine. Be that way!"
+      true -> "Whatever."
     end
+  end
+
+  # Final character is a ?
+  defp asked_question?(input) do
+    String.match?(input, ~r/\?$|\?\s+?/)
+  end
+
+  # All alpha characters made up of uppercase characters
+  defp yelled_at?(input) do
+    String.match?(input, ~r/^[[:punct:][:upper:]]+$/u)
+  end
+
+  # Final charcter is a ? AND is made up of uppercase charcters
+  defp yelled_question?(input) do
+    asked_question?(input) && yelled_at?(input)
+  end
+
+  # ends in a control character, whitespace, or has no content at all
+  defp nonsense_statement?(input) do
+    String.match?(input, ~r/^[[:cntrl:][:blank:]]+$/)
   end
 end
